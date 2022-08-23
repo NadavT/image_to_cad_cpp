ee391eb86ab0691afd40a35e3b8e2d4a6e99eca6
Nadav Tur <t-nadavtur@microsoft.com>
2022-08-24 01:19:04 +0300

    Working dependencies

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include <boost/polygon/voronoi.hpp>
 
 int main(int argc, char** argv )
 {
-
+    boost::polygon::voronoi_diagram<double> vd;
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
     cv::Mat image;  // variable image of datatype Matrix
     image = cv::imread("./OpenCV.png");
 
