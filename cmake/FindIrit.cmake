d3e6cdee8c6dae76c8f487f8d288e44fbf97d718
Nadav Tur <t-nadavtur@microsoft.com>
2022-10-02 00:57:36 +0300

    Added Irit debug handling and irit_dynamic_lib_path

--- cmake/FindIrit.cmake
+++ cmake/FindIrit.cmake
@@ -12,18 +12,42 @@ if(WIN32)
 	)
 
 	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-		find_library(Irit_LIBRARIES NAMES Irit64 HINTS
-			"$ENV{IRIT_PATH64}/../lib64"
-			"$ENV{IRIT_PATH}/../lib64")
+		if(CMAKE_BUILD_TYPE MATCHES Debug)
+			find_library(Irit_LIBRARIES NAMES IritD64 HINTS
+				"$ENV{IRIT_PATH64}/../lib64"
+				"$ENV{IRIT_PATH}/../lib64")
+			find_file(irit_dynamic_lib_path NAMES IritD64.dll HINTS
+				"$ENV{IRIT_PATH64}"
+				"$ENV{IRIT_PATH}")
+		else()
+			find_library(Irit_LIBRARIES NAMES Irit64 HINTS
+				"$ENV{IRIT_PATH64}/../lib64"
+				"$ENV{IRIT_PATH}/../lib64")
+			find_file(irit_dynamic_lib_path NAMES Irit64.dll HINTS
+				"$ENV{IRIT_PATH64}"
+				"$ENV{IRIT_PATH}")
+		endif()
 
 		if(MSVC)
 			add_compile_definitions("WIN64")
 			add_compile_definitions("__WINNT__")
 		endif()
 	else()
-		find_library(Irit_LIBRARIES NAMES Irit HINTS
-			"$ENV{IRIT_PATH64}/../lib"
-			"$ENV{IRIT_PATH}/../lib")
+		if(CMAKE_BUILD_TYPE MATCHES Debug)
+			find_library(Irit_LIBRARIES NAMES IritD HINTS
+				"$ENV{IRIT_PATH64}/../lib64"
+				"$ENV{IRIT_PATH}/../lib64")
+			find_file(irit_dynamic_lib_path NAMES IritD.dll HINTS
+				"$ENV{IRIT_PATH64}"
+				"$ENV{IRIT_PATH}")
+		else()
+			find_library(Irit_LIBRARIES NAMES Irit HINTS
+				"$ENV{IRIT_PATH64}/../lib64"
+				"$ENV{IRIT_PATH}/../lib64")
+			find_file(irit_dynamic_lib_path NAMES Irit.dll HINTS
+				"$ENV{IRIT_PATH64}"
+				"$ENV{IRIT_PATH}")
+		endif()
 
 		if(MSVC)
 			add_compile_definitions("WIN32")
@@ -34,8 +58,22 @@ if(WIN32)
 else()
 	find_path(Irit_INCLUDE_DIRS NAMES inc_irit/irit_sm.h HINTS
 		"$ENV{IRIT_PATH}/..")
-	find_library(Irit_LIBRARIES NAMES Irit HINTS
-		" $ENV{IRIT_PATH}/../lib ")
+
+	if(CMAKE_BUILD_TYPE MATCHES Debug)
+		find_library(Irit_LIBRARIES NAMES IritD HINTS
+			"$ENV{IRIT_PATH}/../lib"
+			"$ENV{IRIT_PATH}")
+		find_file(irit_dynamic_lib_path NAMES IritD.so HINTS
+			"$ENV{IRIT_PATH}"
+			"$ENV{IRIT_PATH}/../lib")
+	else()
+		find_library(Irit_LIBRARIES NAMES Irit HINTS
+			"$ENV{IRIT_PATH}/../lib"
+			"$ENV{IRIT_PATH}")
+		find_file(irit_dynamic_lib_path NAMES Irit.so HINTS
+			"$ENV{IRIT_PATH}"
+			"$ENV{IRIT_PATH}/../lib")
+	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
