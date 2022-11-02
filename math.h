7196a26f35239dcd986d9bea4c7e6aec56c98d88
Nadav Tur <t-nadavtur@microsoft.com>
2022-11-02 12:45:50 +0200

    Correct distance to edge calculation

--- math.h
+++ math.h
@@ -4,24 +4,28 @@
 #include <cmath>
 #include <opencv2/opencv.hpp>
 
+static inline double distance_squared(cv::Point p1, cv::Point p2)
+{
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
 static inline double distance(cv::Point p1, cv::Point p2)
 {
-    return std::sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    return std::sqrt(distance_squared(p1, p2));
 }
 
 static inline double distance_to_edge(cv::Point point, cv::Point edge_start, cv::Point edge_end)
 {
-    double x1 = edge_start.x;
-    double y1 = edge_start.y;
-    double x2 = edge_end.x;
-    double y2 = edge_end.y;
-    double x0 = point.x;
-    double y0 = point.y;
+    double length_squared = distance_squared(edge_start, edge_end);
+    cv::norm(edge_end - edge_start);
+    if (length_squared == 0)
+    {
+        return distance(point, edge_start);
+    }
+    double t = std::clamp((point - edge_start).dot(edge_end - edge_start) / length_squared, 0.0, 1.0);
+    cv::Point projection = edge_start + t * (edge_end - edge_start);
 
-    double numerator = std::abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1));
-    double denominator = std::sqrt(std::pow(y2 - y1, 2) + std::pow(x2 - x1, 2));
-
-    return numerator / denominator;
+    return distance(point, projection);
 }
 
 #endif /* MATH_H */
